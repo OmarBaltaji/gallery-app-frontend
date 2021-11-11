80723340b585858fa23b8e98aa65865f0bd1196e
OmarBaltaji <omr.baltaji@gmail.com>
2021-11-11 23:08:58 +0200

    feat: modified the App vue file

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+  body {
+    padding: 0;
+    margin: 0;
+  }
+
+  .screen-center-items {
+    height: 100vh;
+    display: grid;
+    place-items: center;
+    /* align-items: center; */
+  }
+</style>
