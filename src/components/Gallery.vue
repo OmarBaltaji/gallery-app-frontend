2674f8b9d83c5adbbe31c71c4dd131d6bc516e20
OmarBaltaji <omr.baltaji@gmail.com>
2021-11-14 22:32:46 +0200

    style: adjusted styling of gallery page and added a text to indicate that no galleries have been added by the user as of yet

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,21 +1,20 @@
 <template>
     <div class="p-grid">
         <div v-for="(gallery, index) in galleries" :key = index class="p-col-3">
-            <Card class="text-no-decoration">
+            <Card class="rounded-card">
                     <template #title>
-                        <h3 class="text-no-decoration">{{gallery.name}}</h3>
-                    </template>
-                    <template #content>
-                    <div>{{ gallery.keywords }}</div>
-                    <div>{{ gallery.description }}</div>
+                        <h3 class="p-text-primary">{{gallery.name}}</h3>
                     </template>
                     <template #footer>
                         <div class="p-d-flex p-jc-between p-ai-center">
-                            <Button class="p-button-danger" @click="$emit('delete', gallery.id)" style="z-index: 200">Delete</Button>
-                            <Button class="p-button-secondary" @click="handleRedirect(gallery)">Go to gallery</Button>
+                            <Button class="p-button-secondary p-button-rounded" icon="pi pi-folder-open" @click="handleRedirect(gallery)" label="Open" />
+                            <Button class="p-button-danger p-button-rounded gallery-delete-btn" icon="pi pi-trash" @click="$emit('delete', gallery.id)" />
                         </div>
                     </template>
             </Card>
+        </div>
+        <div v-if="galleries.length === 0" class="p-px-2">
+            <h3>You don't have any galleries yet</h3>
         </div>
     </div>
 </template>
@@ -34,8 +33,25 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
   a {
     text-decoration: none !important;
+  }
+
+  .p-card {
+    background: url('../assets/2059714-200.png') center center no-repeat !important;
+    background-color: #e09393  !important;
+    position: relative;
+  }
+
+  .gallery-delete-btn {
+    position: absolute;
+    top: 5%;
+    right: 5%;
+  }
+
+  h3 {
+    margin-top: 0;
+    margin-bottom: 130px;
   }
 </style>
