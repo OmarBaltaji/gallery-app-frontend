b2d4f71875c549bce28948895f98c110ec7a5ca6
Omar Baltaji <omr.baltaji@gmail.com>
2025-08-22 00:22:45 +0300

    build4

---
+++ src/components/RandomJoke.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="p-4 border rounded bg-yellow-100">
+    <p v-if="joke">{{ joke }}</p>
+    <button @click="getJoke" class="mt-2 px-3 py-1 bg-purple-600 text-white rounded">
+      New Joke
+    </button>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const joke = ref("")
+
+async function getJoke() {
+  const res = await fetch("https://v2.jokeapi.dev/joke/Any?type=single")
+  const data = await res.json()
+  joke.value = data.joke || "No joke today 😅"
+}
+
+onMounted(getJoke)
+</script>
