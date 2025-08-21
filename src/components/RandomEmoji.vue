b1fd7d9a9f77f1d110587bdfcbc4576e1f0e8658
Omar Baltaji <omr.baltaji@gmail.com>
2025-08-22 00:18:57 +0300

    build3

---
+++ src/components/RandomEmoji.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="text-4xl cursor-pointer" @click="newEmoji">
+    {{ emoji }}
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const emojis = ["😀","🎉","🔥","🚀","🍕","🐱","🌍","⚡"]
+const emoji = ref(emojis[0])
+
+function newEmoji() {
+  const randomIndex = Math.floor(Math.random() * emojis.length)
+  emoji.value = emojis[randomIndex]
+}
+</script>
